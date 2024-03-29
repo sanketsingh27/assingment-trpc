6cc4332140cf08f0f0c7b7c6381fc617ea862f7a
sanketsingh27 <singh.sanket27@gmail.com>
2024-03-29 19:53:02 +0530

    show category page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
 import { api } from "~/utils/api";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Layout from "~/components/Layout";
 
 export default function Home() {
   const router = useRouter();
@@ -17,17 +18,32 @@ export default function Home() {
   const { isLoading, data } = api.category.fetchCategories.useQuery();
 
   isLoading ? <h2>Categories are loading</h2> : null;
+
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          {data?.map(({ name, id }) => (
-            <p key={id} className="text-2xl text-white">
-              {name}
-            </p>
-          ))}
+    <Layout>
+      <div className=" m-auto mt-4 flex max-h-[700px] max-w-md	 flex-col  rounded-3xl border border-solid border-stone-300 bg-white px-12 pb-12 pt-8 text-base max-md:px-5">
+        <div className="self-center text-2xl font-semibold text-black">
+          Please mark your interests!
         </div>
-      </main>
-    </>
+
+        <div className="self-center text-xl font-normal  text-black">
+          We will keep you notified.
+        </div>
+
+        <div className=" h-full overflow-scroll scroll-smooth">
+          {data?.map(({ name, id }) => {
+            return (
+              <label key={id} className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5  rounded-md checked:accent-black  "
+                />
+                <span className=" text-lg text-black">{name}</span>
+              </label>
+            );
+          })}
+        </div>
+      </div>
+    </Layout>
   );
 }
