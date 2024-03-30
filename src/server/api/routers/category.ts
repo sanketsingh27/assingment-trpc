e2ee5afdc0f07eac8be74e08ad8cabac66c30881
sanketsingh27 <singh.sanket27@gmail.com>
2024-03-30 17:44:03 +0530

    BE pagination

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,8 +1,20 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
 
 export const categoryRouter = createTRPCRouter({
-  fetchCategories: publicProcedure.query(async ({ ctx }) => {
-    const categories = await ctx.db.category.findMany();
-    return categories;
-  }),
+  fetchCategories: publicProcedure
+    .input(
+      z.object({
+        pageNo: z.number().min(1).default(1),
+        take: z.number().min(1).max(100).default(6), //
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const { pageNo, take } = input;
+      const categories = await ctx.db.category.findMany({
+        skip: (pageNo - 1) * take,
+        take,
+      });
+      return categories;
+    }),
 });
